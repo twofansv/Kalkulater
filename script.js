c4db06efbbfd3fca08c2ae9aa9986435c88c8ac3
twofansv <95734976+twofansv@users.noreply.github.com>
2026-04-28 21:27:43 +0800

    [^] Update the variable naming of the display output to displayOutput. Ongoing code draft for Addition logic

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-// START 
-// Set a const for a two number -> firstNum and secondNum
-// Set a const for calculation
-// User presses a number button and display in output then put it in firstNum
-// User presses an operator for example ADDITION then awaits the second number
-// IF both firstNum and secondNum have been assigned 
-
-
-
-
-
-
-
-
-
 let numberButtons = document.querySelectorAll('.number');
 
-const output = document.querySelector('#output');
-const numberBox = 0;
+const displayOutput = document.querySelector('#output');
+const clear = document.querySelector('#clear');
+let calculationBox = 0;
 
 const add = document.querySelector('#add');
-const clear = document.querySelector('#clear');
 
 
 
@@ -29,26 +14,24 @@ clear.addEventListener('click', () => {
 });
 
 
-// const numsArr = Array.from(nums);
-
-// const out = numsArr.map((item) => {
-//     return item.textContent;
-// });
-
-//Epiphany: IF button is not equals to the .textContent -> continue on BUT if it matches then show it to .output
 numberButtons.forEach((button) => {
-        button.addEventListener('click', () => {
-            output.textContent += button.textContent;
-        })
-
+    button.addEventListener('click', () => {
+        if (output.textContent === '0') {
+        displayOutput.textContent = '';
+        }
+        displayOutput.textContent += button.textContent;
+        calculationBox += displayOutput.textContent;
+    });
 });
 
+add.addEventListener('click', () => {
+        if (output.textContent === output.textContent) {
+            output.textContent += '+';
+        }
+});
+ 
 
 
 
 
 
-
-
-    // const numArr = Array.from(nums.textContent).join('').split('\n').join('').split(',').join('');
-   
