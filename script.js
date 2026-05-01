1d8b89efc02ba8e377535d3388057938b2a68911
twofansv <95734976+twofansv@users.noreply.github.com>
2026-05-01 14:55:30 +0800

    [^] Made the logic for addition calculation

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let numberButtons = document.querySelectorAll('.number');
+let operatorButtons = document.querySelectorAll('#button-operators')
 
-const displayOutput = document.querySelector('#output');
+let displayOutput = document.querySelector('#output');
 const clear = document.querySelector('#clear');
 let firstNum = 0;
 let secondNum = 0;
 
 const add = document.querySelector('#add');
-
+const equals = document.querySelector('#equals-button');
 
 
 
@@ -16,32 +17,44 @@ clear.addEventListener('click', () => {
     secondNum = 0;
 });
 
-function operate () {
-   
-};
-//epiphany: if may na click an ANY operator, bali yung magiging container ng 
+
+//epiphany: if may na click na ANY operator, bali yung magiging container ng 
 //second number is yung secondNum na.
-if (firstNum === 0){
-    numberButtons.forEach((button) => {
-        button.addEventListener('click', () => {
-            if (output.textContent === '0') {
-                displayOutput.textContent = '';
-            }
-                displayOutput.textContent += button.textContent;
-                firstNum = displayOutput.textContent;
-        });
+
+//GET firstNum:
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (output.textContent === '0') {
+            displayOutput.textContent = '';
+        }
+            displayOutput.textContent += button.textContent;
+            firstNum = displayOutput.textContent;
     });
-} else if (firstNum > 0) {
-    numberButtons.forEach((button) => {
-        button.addEventListener('click', () => {
-            if (output.textContent === '0') {
-                displayOutput.textContent = '';
-            }
-                displayOutput.textContent += button.textContent;
-                secondNum = displayOutput.textContent;
+});
+
+//GET operator:
+
+add.addEventListener('click', () => {
+    displayOutput.textContent += '+';
+
+    equals.addEventListener('click', () => {
+        let expression = displayOutput.textContent;
+
+        const splitted = expression.split('+');
+
+        const result = splitted.reduce((previous, curr) => {
+            // kaya may + yung previous at curr is to convert it to number. 
+            return +previous + +curr;
         });
+        displayOutput.textContent = '';
+        displayOutput.textContent += result;
     });
-};
+    
+});
+
+
+
+
 
 
 
